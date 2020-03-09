57947e642d44b93d96458e4399955374abacc9db
Antonio Simões <antonio.simoes@cohab.sp.gov.br>
2020-03-09 16:54:31 -0300

    Adicionado models com tabelas de pessoas e atividades e criado um utils de teste para manipular os dados do banco de dados!

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker, relationship #são resp
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('sqlite:///atividades.db', convert_unicode=True)
-db_session = scoped_session(sessionmaker(autocommit=False, bind=engine))
+db_session = scoped_session(sessionmaker(autocommit=False, binds=engine))
 
 Base = declarative_base()
 Base.query = db_session.query_property() #serve para criar e trabalhar com o db
@@ -13,11 +13,19 @@ Base.query = db_session.query_property() #serve para criar e trabalhar com o db
 class Pessoas(Base):
     __tablename__='pessoas'
     id = Column(Integer, primary_key=True)
-    nome = Column(String(40), index=True) #deixa a columa mais rápida na busca
+    nome = Column(String(40), index=True) #deixa as consultas mais rápidas na busca
     idade = Column(Integer)
     
     def __repr__(self):
         return '<Pessoa {}>'.format(self.nome)
+    
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+    
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
 
 class Atividades(Base):
     __tablename__='atividades'
@@ -27,5 +35,19 @@ class Atividades(Base):
     pessoa_id = Column(Integer, ForeignKey('pessoas.id'))
     pessoa = relationship("Pessoas")
     
+    def __repr__(self):
+        return '<Atividades {}>'.format(self.nome)
+    
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+    
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
+    
 def init_db():
-    Base.metadata.create_all(bind=engine)
+    Base.metadata.create_all(bind=engine) #vai criar o banco de dados
+    
+if __name__ == '__main__':
+    init_db()
--- utils.py
+++ utils.py
@@ -3,12 +3,27 @@ from models import Pessoas
 def insere_pessoas():
     pessoa = Pessoas(nome='Rafael', idade=29)
     print(pessoa)
-
-def consulta():
-    #pessoa = Pessoas.query.filter_by(nome='Rafael')
-    pessoa = Pessoas.query.all()
+    pessoa.save()
+    pessoa = Pessoas(nome='Ronaldo', idade=40)
     print(pessoa)
+    pessoa.save()
+    
+def consulta_pessoas():    
+    pessoa = Pessoas.query.all()
+    pessoa = Pessoas.query.filter_by(nome='Rafael').first()
+    print(pessoa)
+    
+def altera_pessoa():
+    pessoa = Pessoas.query.filter_by(nome="Ronaldo").first()
+    pessoa.idade = 46
+    pessoa.save()
+    
+def exclui_pessoa():
+    pessoa = Pessoas.query.filter_by(nome="Rafael").first()
+    pessoa.delete()
 
 if __name__ == '__main__':
-    #insere_pessoas()
-    consulta()
+    # insere_pessoas()
+    # consulta_pessoas()
+    # altera_pessoa()
+    # exclui_pessoa()
