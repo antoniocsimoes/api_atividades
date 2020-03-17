f90ba9cad1ae6f3d59d1d240210c9f27e9169bef
Antonio Simões <antonio.simoes@cohab.sp.gov.br>
2020-03-17 14:18:03 -0300

    Alterado nossa REST API para realizar autenticaçao no banco de dados. Em models foi criado tabela de usuarios com login e senha para autenticaçao nas APIs. Incluido em requeriments a extensao flaks-httpauth.

--- models.py
+++ models.py
@@ -1,28 +1,26 @@
-#classes que irão referenciar as tabelas
-
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import scoped_session, sessionmaker, relationship #são responsaveis por criar a sessão
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('sqlite:///atividades.db', convert_unicode=True)
-db_session = scoped_session(sessionmaker(autocommit=False, binds=engine))
-
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         bind=engine))
 Base = declarative_base()
-Base.query = db_session.query_property() #serve para criar e trabalhar com o db
+Base.query = db_session.query_property()
 
 class Pessoas(Base):
     __tablename__='pessoas'
     id = Column(Integer, primary_key=True)
-    nome = Column(String(40), index=True) #deixa as consultas mais rápidas na busca
+    nome = Column(String(40), index=True)
     idade = Column(Integer)
-    
+
     def __repr__(self):
         return '<Pessoa {}>'.format(self.nome)
-    
+
     def save(self):
         db_session.add(self)
         db_session.commit()
-    
+
     def delete(self):
         db_session.delete(self)
         db_session.commit()
@@ -31,23 +29,40 @@ class Atividades(Base):
     __tablename__='atividades'
     id = Column(Integer, primary_key=True)
     nome = Column(String(80))
-    
     pessoa_id = Column(Integer, ForeignKey('pessoas.id'))
     pessoa = relationship("Pessoas")
-    
+
     def __repr__(self):
         return '<Atividades {}>'.format(self.nome)
-    
+
     def save(self):
         db_session.add(self)
         db_session.commit()
-    
+
     def delete(self):
         db_session.delete(self)
         db_session.commit()
-    
+
+class Usuarios(Base):
+    __tablename__='usuarios'
+    id = Column(Integer, primary_key=True)
+    login = Column(String(20), unique=True)
+    senha = Column(String(20))
+
+    def __repr__(self):
+        return '<Usuario {}>'.format(self.login)
+
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
+
 def init_db():
-    Base.metadata.create_all(bind=engine) #vai criar o banco de dados
-    
+    Base.metadata.create_all(bind=engine)
+
 if __name__ == '__main__':
     init_db()
+
